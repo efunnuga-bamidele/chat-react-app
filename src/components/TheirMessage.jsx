55ddd370c44b52a87dd293233274c8eed9f33454
Efunnuga Bamidele <bamideleefunnuga9@gmail.com>
2022-10-25 15:48:44 +0100

    Implement TheirMessage.jsx functions

--- src/components/TheirMessage.jsx
+++ src/components/TheirMessage.jsx
@@ -1,7 +1,28 @@
-const TheirMassage = () => {
+const TheirMassage = ({ message, lastMessage }) => {
+    const isFirstMessageByUser = !lastMessage || lastMessage.sender.userName !== message.sender.userName;
     return(
-        <div>
-            Their Message
+        <div className="message-row">
+            {isFirstMessageByUser && (
+                <div 
+                    className="message-avater"
+                    style={{backgroundImage: message.sender && `url(${message.sender.avater})`}}
+                />
+            )}
+            {message.attachment && message.attachment.lenght > 0 
+                ?(
+                    <img 
+                        src={message.attachments.file} 
+                        alt="message-attachment" 
+                        className="message-image"
+                        style={{ merginLeft: isFirstMessageByUser ? '4px' : '48px'}}
+                    />
+                )
+                :(
+                    <div className="message" style={{ float: 'left', backgroundColor: '#CABCDC', marginLeft: isFirstMessageByUser ? '4px' : '48px' }}>
+                        {message.text}
+                    </div>
+                )
+            }
         </div>
     )
 }
