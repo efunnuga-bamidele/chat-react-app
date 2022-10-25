d82eff41bc800d3bc8f8298d9ee66df01303e899
Efunnuga Bamidele <bamideleefunnuga9@gmail.com>
2022-10-25 15:00:24 +0100

    Create Components Structure in jsx file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-
+import { ChatEngine } from 'react-chat-engine'
+import ChatFeed from './components/ChatFeed.jsx'
 //Style
 import './App.css'
 
 export default function App(){
     return(
-        <div>
-            <h1>Hello React</h1>
+        <div className='App'>
+            <ChatEngine
+                height="100vh"
+                projectID="b2bcdee6-a99c-47a1-86b0-af9556988322"
+                userName="john"
+                userSecret="password"
+                renderChatFeed = {(chatAppProps) => <ChatFeed { ...chatAppProps} />
+                }
+                onNewMessage = {() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3')}
+            />
+            
         </div>
     )
 }
